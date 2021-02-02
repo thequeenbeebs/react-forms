eb1a4dc8bfca1dca124e6eacca5cc46eeef2b3cc
Blaire Baker <bakerblaire@gmail.com>
2021-02-02 17:45:28 -0600

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,57 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
-  }
+  // state = {
+  //   firstName: "John",
+  //   lastName: "Henry",
+  //   submittedData: []
+  // }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
+  // handleFirstNameChange = event => {
+  //   this.setState({
+  //     firstName: event.target.value
+  //   })
+  // }
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
+  // handleLastNameChange = event => {
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+
+  // handleSubmit = event => {
+  //   event.preventDefault()
+  //   let formData = { firstName: this.state.firstName, lastName: this.state.lastName}
+  //   let dataArray = this.state.submittedData.concat(formData)
+  //   this.setState({submittedData: dataArray})
+  // }
+
+  // listOfSubmissions = () => {
+  //   return this.state.submittedData.map(data => {
+  //     return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+  //   })
+  // }
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+            name="firstName"
+          />
+          <input
+            type="text"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName}
+            name="lastName"
+          />
+        </form>
+        {/* {this.listOfSubmissions()} */}
+      </div>
+      
     )
   }
 }
